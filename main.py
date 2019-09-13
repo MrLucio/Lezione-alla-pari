ea3ee391fdd7c349feb11eac3ebd385c77b2fa2e
MrLucio <MrLucio@users.noreply.github.com>
2019-09-13 22:26:11 +0200

    deleted old files and directories inside root path

--- main.py
+++
@@ -1,7 +0,0 @@
-# -- IMPORTS
-from CourseApplication import CourseApplication
-from LoginApplication import LoginApplication
-from UserManager import UserManager
-
-if __name__ == "__main__":
-    a = CourseApplication(UserManager(), "u-1")
